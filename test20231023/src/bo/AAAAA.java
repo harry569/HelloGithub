1bc08ff6a95d82de2b0adeffc2e6e69f5bf2bcf0
harry569 <646818692@qq.com>
2023-10-27 15:18:30 +0800

    20231027

--- test20231023/src/bo/AAAAA.java
+++ test20231023/src/bo/AAAAA.java
@@ -1,7 +1,14 @@
 package bo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AAAAA {
  public static void main(String[] args) {
-	System.out.println("1234");
+	Map<String, Object> map = new HashMap<String, Object>();
+	map.put("result","1");
+	map.put("key", "2");
+	System.out.println(map.toString());
+	System.out.println(map.get("result"));
   }
 }
